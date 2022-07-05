1400dcc891ec8bf0296baf704628bcde4ec04f9c
Z0R007 <108648440+Z0R007@users.noreply.github.com>
2022-07-05 11:43:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import snowflake.connector
 import pandas
 import requests
-from urllib.error import URLERROR
+from urllib.error import URLError
 
 streamlit.title('My Mom''s new healthy Diner')
 
